90d87bc20aa0372a8bc4d8d46d38df33dd563800
Papatoncio <juanpij23@hotmail.com>
2024-10-21 21:39:19 -0600

    Firebase Cloud Messaging Implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
+import 'package:capistock/api/firebaseapi.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  await FirebaseApi().initNotifications();
+
   runApp(MyApp());
 }
 
