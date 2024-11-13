340a441add4a7be72c10bba19a3dd4fb166e729c
AlexRGB <alexismtz200326@gmail.com>
2024-11-13 17:09:22 -0600

    Refactor menú lateral y mejora de estilos en el tema de la aplicación

--- lib/util/apptheme.dart
+++ lib/util/apptheme.dart
@@ -2,48 +2,53 @@ import 'package:flutter/material.dart';
 
 class Apptheme {
   final themeData = ThemeData(
-      primaryColor: const Color(0xFFEE891A), // Color principal (#ed4103)
-      primaryColorLight: const Color(0xFFEE891A), // Color de acento (#ee891a)
-      scaffoldBackgroundColor:
-          const Color(0xFF132436), // Fondo principal (#132436)
-      textTheme: const TextTheme(
-        bodyLarge: TextStyle(
-          color: Color(0xFF132436),
-          backgroundColor: Color(0xFFF8FADD),
-        ), // Texto blanco por defecto
-        bodyMedium: TextStyle(
-          color: Color(0xFF132436),
-          backgroundColor: Color(0xFFF8FADD),
-        ),
-        bodySmall: TextStyle(
-          color: Color(0xFF132436),
-          backgroundColor: Color(0xFFF8FADD),
-        ),
+    primaryColor: const Color(0xFFEE891A), // Color principal (#ed4103)
+    primaryColorLight: const Color(0xFFEE891A), // Color de acento (#ee891a)
+    scaffoldBackgroundColor:
+        const Color(0xFF132436), // Fondo principal (#132436)
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(
+        color: Color(0xFF132436),
+        backgroundColor: Color(0xFFF8FADD),
+      ), // Texto blanco por defecto
+      bodyMedium: TextStyle(
+        color: Color(0xFF132436),
+        backgroundColor: Color(0xFFF8FADD),
       ),
-      inputDecorationTheme: InputDecorationTheme(
-        filled: true,
-        fillColor:
-            const Color(0xFFF8FADD), // Fondo de los campos de texto (#f8fadd)
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8.0),
-          borderSide: BorderSide.none,
+      bodySmall: TextStyle(
+        color: Color(0xFF132436),
+        backgroundColor: Color(0xFFF8FADD),
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor:
+          const Color(0xFFF8FADD), // Fondo de los campos de texto (#f8fadd)
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        borderSide: BorderSide.none,
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: const TextStyle(
+          color: Color(0xFFEE891A), // Botón con color (#ee891a)
         ),
-      ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          textStyle: const TextStyle(
-            color: Color(0xFFEE891A), // Botón con color (#ee891a)
-          ),
-          backgroundColor: const Color(0xFFF8FADD),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+        backgroundColor: const Color(0xFFF8FADD),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
         ),
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
       ),
-      appBarTheme: const AppBarTheme(
-        backgroundColor:
-            Color(0xFFEE891A), // Color de la barra superior (#ed4103)
-      ),
-      drawerTheme: const DrawerThemeData(backgroundColor: Color(0xFFEE891A)));
+    ),
+    appBarTheme: const AppBarTheme(
+      backgroundColor:
+          Color(0xFFEE891A), // Color de la barra superior (#ed4103)
+    ),
+    drawerTheme: const DrawerThemeData(
+      backgroundColor: Color(0xFFEE891A),
+      shadowColor: Color(0xFF132436),
+      elevation: 16,
+    ),
+  );
 }
